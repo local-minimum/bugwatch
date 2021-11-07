ae3551c2ed02556048eb20b872abd86884a9e587
Martin Zackrisson <martin.zackrisson@fripost.org>
2021-11-07 11:11:38 +0100

    Attempting input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -32,6 +33,33 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    private void OnEnable()
+    {
+        GetComponent<PlayerInput>().onActionTriggered += PlayerController_onActionTriggered;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<PlayerInput>().onActionTriggered -= PlayerController_onActionTriggered;
+    }
+
+    private void PlayerController_onActionTriggered(InputAction.CallbackContext context)
+    {
+        if (!ready) return;
+        var action = context.action;
+        switch (action.name)
+        {
+            case "Move":
+                OnMove(context.ReadValue<Vector2>());
+                break;
+            case "Interact":
+                break;
+            case "Look":
+                break;
+        }
+        
+    }
+
     IEnumerator<WaitForEndOfFrame> WaitForLoaded(AsyncOperation operation)
     {
         while (!operation.isDone)
@@ -43,35 +71,35 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Update()
-    {
-        if (!ready) return;
-        UpdateMove();
-        UpdateRotation();
-        UpdateUpDownLook();
+    {        
+        if (!ready) return;        
         UpdateLookAt();
     }
 
-    private void UpdateMove()
-    {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        var moveVector = transform.forward * vertical + transform.right * horizontal;
-        moveVector = Vector3.ClampMagnitude(moveVector, 1);
-        _controller.Move(moveVector * speed * Time.deltaTime);
+    public void OnMove(Vector2 moveVector)
+    {        
+        _controller.Move(moveVector.normalized * speed * Time.deltaTime);
     }
 
-    private void UpdateRotation()
+    public void OnLook(InputValue input)
     {
-        var a = Input.GetAxis("Mouse X") * mouseAngleSpeed * Time.deltaTime;
+        Vector2 lookVector = input.Get<Vector2>();
+        UpdateRotation(lookVector.x);
+        UpdateUpDownLook(lookVector.y);
+
+    }
+
+    private void UpdateRotation(float x)
+    {                
         var rotation = transform.rotation;
-        rotation *= Quaternion.AngleAxis(a, Vector3.up);
+        rotation *= Quaternion.AngleAxis(x, Vector3.up);
         transform.rotation = rotation;
     }
 
-    private void UpdateUpDownLook()
+    private void UpdateUpDownLook(float y)
     {
         _upDownRotation = Mathf.Clamp(
-            _upDownRotation - Input.GetAxis("Mouse Y") * mouseAngleSpeed * Time.deltaTime,
+            _upDownRotation - y * mouseAngleSpeed * Time.deltaTime,
             lookMinRotation,
             lookMaxRotation
         );
