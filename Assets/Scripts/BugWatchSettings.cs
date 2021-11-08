62634b30660619a4048a48e8cb33c1dce00597a4
Martin Zackrisson <martin.zackrisson@fripost.org>
2021-11-08 20:20:38 +0100

    Simple main menu

--- Assets/Scripts/BugWatchSettings.cs
+++ Assets/Scripts/BugWatchSettings.cs
@@ -6,15 +6,16 @@ using System.Reflection;
 
 public static class BugWatchSettings
 {
-    static bool _verbose = false;
+    static bool _verbose = true;
     private static void _ResetSection(string section)
     {
         var start = string.Format("{0}.", section);
         var flags = BindingFlags.Static | BindingFlags.NonPublic;
+
         var fields = typeof(BugWatchSettings)
             .GetFields(flags)
             .Where(f => f.FieldType == typeof(string))
-            .Select(f => f.Name)
+            .Select(f => f.GetValue(null).ToString())
             .Where(fName => fName.StartsWith(start))
             .ToList();
         foreach (string field in fields)
@@ -64,6 +65,48 @@ public static class BugWatchSettings
         PlayerPrefs.SetInt(CollectableKey(collectable), 42);
     }
 
-    public static void ResetInventory() => _ResetSection(_INVENTORY);
+    public static void ResetInventory()
+    {
+        _ResetSection(_INVENTORY);
+        foreach (CollectableType collectable in System.Enum.GetValues(typeof(CollectableType)))
+        {
+            var key = CollectableKey(collectable);
+            if (PlayerPrefs.HasKey(key))
+            {
+                if (_verbose) Debug.Log(string.Format("Removing setting for {0}", key));
+                PlayerPrefs.DeleteKey(key);
+            }
+        }
+    }
+    #endregion
+
+    #region
+    private static readonly string _PROGRESS = "Progress";
+    private static readonly string _PROGRESS_HAS_GAME = string.Format("{0}.HasGame", _PROGRESS);
+    private static readonly string _PROGRESS_LEVEL = string.Format("{0}.Level", _PROGRESS);
+    public static bool HasProgress
+    {
+        get
+        {
+            return PlayerPrefs.HasKey(_PROGRESS_HAS_GAME);
+        }
+    }
+
+    public static string Level
+    {
+        get
+        {
+            return PlayerPrefs.GetString(_PROGRESS_LEVEL, "Scenes/DemoScene");
+        }
+    }
+
+    public static void ResetProgress() => _ResetSection(_PROGRESS);
+    public static void NewGame()
+    {
+        ResetProgress();
+        ResetInventory();
+        PlayerPrefs.SetInt(_PROGRESS_HAS_GAME, 42);
+
+    }
     #endregion
 }
---
+++ Assets/Scripts/UI/UIMain.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIMain : MonoBehaviour
+{    
+    [SerializeField]
+    Button Resume;
+
+    [SerializeField]
+    Button NewGame;
+
+    [SerializeField]
+    Button Settings;
+
+    [SerializeField]
+    Button Quit;
+
+    private void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer) {
+            Quit.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        Resume.gameObject.SetActive(BugWatchSettings.HasProgress);
+    }
+    
+    public void OnResume()
+    {
+        SceneManager.LoadScene(BugWatchSettings.Level);
+    }
+
+    public void OnNewGame()
+    {
+        BugWatchSettings.NewGame();
+        OnResume();
+    }
+
+    public void OnSettings()
+    {
+        Debug.LogError("Not implemented");
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}
